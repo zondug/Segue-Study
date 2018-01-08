2c5b8dea133ad12f34c7aa7624fa62793969e1ff
Zondug Kim <zondug@me.com>
2018-01-08 15:18:56 +0700

    Initial Commit

---
+++ Segue Study/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Segue Study
+//
+//  Created by Zondug Kim on 2018. 1. 8..
+//  Copyright © 2018년 Zondug Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
