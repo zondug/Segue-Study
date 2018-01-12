ed335fc348425daff6bdaf9e5ef5b2d50acb7033
Zondug Kim <zondug@me.com>
2018-01-12 17:35:02 +0700

    comments for myself

--- Segue Study/SecondViewController.swift
+++ Segue Study/SecondViewController.swift
@@ -8,13 +8,14 @@
 
 import UIKit
 
+// protocol 정의. 보통은 파일을 따로 뺀다고 함.
 protocol CanReceive {
-	
 	func dataReceived(data: String)
 }
 
 class SecondViewController: UIViewController {
 
+	// delegate는 protocol에서 받아오는 것
 	var delegate: CanReceive?
 	var data = ""
 	
@@ -22,6 +23,7 @@ class SecondViewController: UIViewController {
 		
 		super.viewDidLoad()
 		
+		// 1 -> 2로 넘어온 data를 label에 넣음
 		label.text = data
 		
 	}
@@ -29,7 +31,10 @@ class SecondViewController: UIViewController {
 	
 	@IBAction func button(_ sender: UIButton) {
 		
+		// 데이터를 델리게이트로 넘김
 		delegate?.dataReceived(data: label.text!)
+		
+		// 현재 화면을 dismiss 
 		dismiss(animated: true, completion: nil)
 
 	}
--- Segue Study/ViewController.swift
+++ Segue Study/ViewController.swift
@@ -17,20 +17,24 @@ class ViewController: UIViewController, CanReceive {
 		
 	}
 
+	// 오브젝트 정의
 	@IBOutlet var textField: UITextField!
 	@IBOutlet var receivedData: UILabel!
 	
+	// 버튼 액션 정의 1 -> 2
 	@IBAction func buttonPressed(_ sender: UIButton) {
 		
 		performSegue(withIdentifier: "gotoSecondScreen", sender: self)
 		 
 	}
 	
+	// 2 -> 1 일 때 호출되는 내용들, from protocol
 	func dataReceived(data: String) {
 		receivedData.isHidden = false
 		receivedData.text = data
 	}
 	
+	// 입력 받은 데이터를 2로 넘기기 전에 준비. segue identifier를 통해서 구분을 하므로, identifier를 여러 개 만들 수 있음 
 	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 		if segue.identifier == "gotoSecondScreen" {
 			
